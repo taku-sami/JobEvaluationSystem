21ff75586f23b4cb6bcad9870be5c6add19894b6
TAKU <takuya062090@gmail.com>
2020-01-16 19:12:24 +0800

    fixed url for sending mail

--- resources/views/auth/email/pre_register.blade.php
+++ resources/views/auth/email/pre_register.blade.php
@@ -1,4 +1,4 @@
 Web 人事考課システムへのユーザー仮登録が完了しました。<br>
 <br>
 以下のURLからログインして、本登録を完了させてください。<br>
-{{url('register/verify/'.$token)}}
+{{('http://job-evaluation-system.herokuapp.com/register/verify/'.$token)}}
